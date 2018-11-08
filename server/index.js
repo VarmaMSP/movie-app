3ffc5fa8589ce3f0bab2ba1f78eaafd259f06807
Pavan Varma <varmamsp111@gmail.com>
2018-11-08 13:27:17 +0530

    Remove checks for env.

--- server/index.js
+++ server/index.js
@@ -2,15 +2,4 @@ require('@babel/register')
 require('dotenv').config()
 require('isomorphic-fetch')
 
-let validEnv = true
-
-if (!process.env.TMDB_API_KEY) {
-  validEnv = false
-  console.log('Cannot find TMDB api key.')
-}
-
-if (validEnv) {
-  require('./server')
-} else {
-  process.exit(1)
-}
+require('./server')
