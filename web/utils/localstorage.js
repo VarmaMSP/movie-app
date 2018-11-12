c334bba04f5aed5d1f452ef9db82041f95705434
Pavan Varma <varmamsp111@gmail.com>
2018-11-12 09:06:35 +0530

    Add utility function for input validation and localstorage access.

---
+++ web/utils/localstorage.js
@@ -0,0 +1,29 @@
+// @flow
+import type { State } from 'types/store'
+
+export const loadLoginData = () => {
+  let loggedInUser
+  try {
+    loggedInUser = localStorage.getItem('logged-in-user')
+  } catch (err) {
+    console.log(err)
+  }
+  return {
+    entities: {
+      profile: {
+        loggedInUser: loggedInUser
+          ? JSON.parse(loggedInUser)
+          : null
+      }
+    }
+  }
+}
+
+export const saveLoginData = (state: State) => {
+  try {
+    let loggedInUser = JSON.stringify(state.entities.profile.loggedInUser)
+    localStorage.setItem('logged-in-user', loggedInUser)
+  } catch (err) {
+    console.log(err)
+  }
+}
