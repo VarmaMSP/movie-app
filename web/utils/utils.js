20956918f090d632507aa405bede277c83cd2765
Pavan Varma <varmamsp111@gmail.com>
2018-11-13 01:25:01 +0530

    Ignore eslint warning.

--- web/utils/utils.js
+++ web/utils/utils.js
@@ -3,6 +3,6 @@
 // Most rated answer for how to validate a email address.
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 export function isValidEmail (email: string): boolean {
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ // eslint-disable-line
   return re.test(email)
 }
