3706d257b3de5d9e548e14af083e2d29caceb15d
Pavan Varma <varmamsp111@gmail.com>
2018-11-14 05:05:46 +0530

    Refactor web.js

--- web/types/store.js
+++ web/types/store.js
@@ -1,8 +1,10 @@
 // @flow
+import type { Store as ReduxStore } from 'redux'
 import type { ApiState } from 'types/api'
 import type { ActorState } from 'types/actor'
 import type { MovieState } from 'types/movie'
 import type { ProfileState } from 'types/profile'
+import type { Action, Dispatch } from 'types/action'
 
 export type State = {|
   entities: {|
@@ -12,3 +14,5 @@ export type State = {|
     profile: ProfileState
   |}
 |}
+
+export type Store = ReduxStore<State, Action, Dispatch>
--- web/web.js
+++ web/web.js
@@ -1,7 +1,5 @@
 // @flow
-import type { Store } from 'redux'
-import type { State } from 'types/store'
-import type { Action, Dispatch } from 'types/action'
+import type { Store } from 'types/store'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -9,7 +7,7 @@ import thunk from 'redux-thunk'
 import { Layout, message } from 'antd'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect, withRouter } from 'react-router-dom'
 
 import Sidebar from 'components/sidebar'
 import Profile from 'components/profile'
@@ -23,30 +21,34 @@ import { loadLoginData, saveLoginData } from 'utils/localstorage'
 
 message.config({ maxCount: 4 })
 
-const store: Store<State, Action, Dispatch> = createStore(reducers, (loadLoginData(): any), applyMiddleware(thunk))
+const store: Store = createStore(reducers, (loadLoginData(): any), applyMiddleware(thunk))
+
+store.subscribe(() => saveLoginData(store.getState()))
+
+const AppContent = withRouter(() => (
+  <>
+    <RedirectIfLoggedout />
+    <Switch>
+      <Route path='/login' component={LoginModal} />
+      <Route path='/profile/:userId?' component={Profile} />
+      <Route path='/movie/:movieId' component={MovieDetails} />
+      <Route path='/discover' component={Discover} />
+      <Route path='/results' component={SearchResults} />
+      <Redirect to='/discover' />
+    </Switch>
+  </>
+))
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Layout>
         <Sidebar />
-        <RedirectIfLoggedout />
         <Layout style={{ marginLeft: 220, height: '100vh', background: '#fff' }}>
-          <Switch>
-            <Route path='/login' component={LoginModal} />
-            <Route path='/profile/:userId?' component={Profile} />
-            <Route path='/movie/:movieId' component={MovieDetails} />
-            <Route path='/discover' component={Discover} />
-            <Route path='/results' component={SearchResults} />
-            <Redirect to='/discover' />
-          </Switch>
+          <AppContent />
         </Layout>
       </Layout>
     </Router>
   </Provider>,
   (document.getElementById('app'): any)
 )
-
-store.subscribe(() => saveLoginData(store.getState()))
-
-store.subscribe(() => console.log(store.getState()))
