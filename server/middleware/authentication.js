947cab11302860bd53f3ce37416721ac3e94d3c8
Pavan Varma <varmamsp111@gmail.com>
2018-11-14 04:24:17 +0530

    FIX BUG

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -2,7 +2,7 @@ export default (req, res, next) => {
   if (!req.session.user) {
     res.status(401).json({
       error: true,
-      message: 'Not Authorized, please login.'
+      message: ['Not Authorized, please login.']
     })
     return
   }
